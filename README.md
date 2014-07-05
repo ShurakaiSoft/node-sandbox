bb486cb143b6e75335f2d4a9e7932970fda386d4
Stephen Parker <shurakaisoft@gmail.com>
2014-07-05 17:15:22 +1000

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ node-sandbox
 ============
 
 A node.js sandbox server for quick testing of new ideas.
+
+
+# Contents
+
+package.json - a barebones example to quickly kickstart dependencies
+
+server.js - the sandbox. Slap some code in here and try some cool new stuff out.
---
+++ server.js
@@ -0,0 +1,16 @@
+/**
+ * Sandbox Node.js Server
+ *
+ * A lightweight seed for quickly testing some cool new node.js feature or idea.
+ */
+
+// dependencies
+var winston = require('winston'),
+	colors = require('colors');
+
+
+// variables
+var message = 'Hello World.';
+
+// test code
+console.log('Sandbox Server says:'.green, message);
